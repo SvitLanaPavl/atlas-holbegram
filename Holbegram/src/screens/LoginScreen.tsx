1fdc02355988567c9b05a31e4161f780a5a7e6b5
Svitlana Pavlovska <lanapavlovska90@gmail.com>
2024-10-12 15:24:09 -0500

    the register and login screens frontend looks fine

---
+++ Holbegram/src/screens/LoginScreen.tsx
@@ -0,0 +1,114 @@
+import React, { useState } from 'react';
+import { View, Text, TextInput, TouchableOpacity, StyleSheet, Image } from 'react-native';
+import auth from '@react-native-firebase/auth';
+import { StackNavigationProp } from '@react-navigation/stack';
+import logo from '../assets/atlas-school.png';
+
+type RootStackParamList = {
+  Login: undefined;
+  Register: undefined;
+  Home: undefined;
+};
+
+type LoginScreenNavigationProp = StackNavigationProp<RootStackParamList, 'Login'>;
+
+type Props = {
+  navigation: LoginScreenNavigationProp;
+};
+
+
+
+const LoginScreen: React.FC<Props> = ({ navigation }) => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const handleLogin = () => {
+    auth().signInWithEmailAndPassword(email, password)
+    .then(() => {
+      navigation.navigate('Home');
+    })
+    .catch(error => setErrorMessage(error.message));
+  };
+
+  return (
+    <View style={styles.container}>
+      <Image source={logo} style={styles.logo} />
+      <Text style={styles.title}>Login</Text>
+      <TextInput
+        style={styles.input}
+        placeholder='Email'
+        placeholderTextColor='#ffffff'
+        value={email}
+        onChangeText={setEmail}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder='Password'
+        placeholderTextColor='#ffffff'
+        value={password}
+        onChangeText={setPassword}
+        secureTextEntry
+      />
+      {errorMessage ? <Text style={styles.error}>{errorMessage}</Text> : null }
+      <TouchableOpacity style={styles.button} onPress={handleLogin}>
+        <Text style={styles.buttonText}>Sign in</Text>
+      </TouchableOpacity>
+      <Text style={styles.link} onPress={() => navigation.navigate('Register')} >Create a new account</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    padding: 20,
+    backgroundColor: '#00003c',
+  },
+  logo: {
+    width: 300,
+    height: 100,
+    resizeMode: 'contain',
+    alignSelf: 'center',
+    marginBottom: 40,
+  },
+  title: {
+    fontSize: 20,
+    marginBottom: 20,
+    color: '#ffffff',
+    textAlign: 'center',
+    fontWeight: 'bold',
+  },
+  input: {
+    height: 40,
+    borderColor: '#1ed2af',
+    borderWidth: 1,
+    marginBottom: 10,
+    paddingHorizontal: 10,
+    borderRadius: 5,
+    color: '#ffffff',
+  },
+  error: {
+    color: 'red',
+  },
+  button: {
+    backgroundColor: '#1ed2af',
+    height: 40,
+    borderRadius: 5,
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  buttonText: {
+    color: '#ffffff',
+    
+  },
+  link: {
+    marginTop: 20,
+    color: '#ffffff',
+    textAlign: 'center',
+  },
+});
+
+export default LoginScreen;
